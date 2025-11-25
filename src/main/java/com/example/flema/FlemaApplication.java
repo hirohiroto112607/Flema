9253637cf1e0cd1c1e7dfaf12085a21004bb31b9
hirohiroto112607 <113352696+hirohiroto112607@users.noreply.github.com>
2025-11-25 13:06:59 +0900

    initial commit

---
+++ src/main/java/com/example/flema/FlemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.flema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flema/FlemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
